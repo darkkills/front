73d405c4a11b110b8065361d2626bf3d6db76c75
Akshaya V R <akshaya123meenu@gmail.com>
2025-07-31 22:55:16 +0530

    first commit

---
+++ habit_hero_backend/settings.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+BASE_DIR = Path(__file__).resolve().parent.parent
+SECRET_KEY = 'dev-secret-key'
+DEBUG = True
+ALLOWED_HOSTS = []
+INSTALLED_APPS = [
+    'django.contrib.admin','django.contrib.auth','django.contrib.contenttypes',
+    'django.contrib.sessions','django.contrib.messages','django.contrib.staticfiles',
+    'rest_framework','habits','corsheaders',
+]
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware','django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware','django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware','django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware','corsheaders.middleware.CorsMiddleware',
+]
+ROOT_URLCONF = 'habit_hero_backend.urls'
+TEMPLATES = [{'BACKEND':'django.template.backends.django.DjangoTemplates','DIRS':[],'APP_DIRS':True,
+    'OPTIONS':{'context_processors':['django.template.context_processors.debug',
+        'django.template.context_processors.request','django.contrib.auth.context_processors.auth',
+        'django.contrib.messages.context_processors.messages',],},},]
+WSGI_APPLICATION = 'habit_hero_backend.wsgi.application'
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',       # Use SQLite
+        'NAME': BASE_DIR / 'habit-hero.sqlite3',      # Your DB filename
+    }
+}
+AUTH_PASSWORD_VALIDATORS = []
+LANGUAGE_CODE='en-us'; TIME_ZONE='UTC'; USE_I18N=True; USE_TZ=True
+STATIC_URL='static/'
+DEFAULT_AUTO_FIELD='django.db.models.BigAutoField'
+CORS_ALLOW_ALL_ORIGINS = True
